b4fe76475b9745f95676339c6f02bd89d27d98f0
Geoegiy Stroganov VM <stroganovgeorgi01@gmail.com>
2023-10-06 17:14:12 +0000

    Добавил фильтр по производителю

--- bmstu_lab/admin.py
+++ bmstu_lab/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Orders
+from .models import Requests
 
+admin.site.register(Orders)
+admin.site.register(Requests)
 # Register your models here.
+    
--- bmstu_lab/views.py
+++ bmstu_lab/views.py
@@ -3,40 +3,22 @@ from bmstu_lab.models import Requests
 from bmstu_lab.models import Users
 from bmstu_lab.models import Moderators
 from bmstu_lab.models import Orders
+from django.db.models import Q
 
 def GetOrders(request):
-    orders_arr = Orders.objects.all()
     input_text = request.GET.get("find")
-    print(input_text)
-    temp_arr = []
-    if input_text is None:
-        return render(request, 'orders.html', {'data' : {
-                'orders': orders_arr,
-                'query': "",
-            }})
-    for i in orders_arr:
-        if input_text is not None:
-            if input_text in i.title:
-                temp_arr.append(i)
-        else:
-            return render(request, 'orders.html', {'data' : {
-                'orders': orders_arr,
-                'query': input_text,
-            }})
-    return render(request, 'orders.html', {'data' : {
-        'orders': temp_arr,
-        'query': input_text,
-    }})
+    input_filter = request.GET.getlist("manufacturer")
+    orders = Orders.objects.all()
+    
+    if input_text: orders = orders.filter(Q(title__icontains = input_text))
+    else: input_text = ''
+
+    if input_filter:
+        filter_list = [Q(processor__icontains=filter_item) for filter_item in input_filter]
+        orders = orders.filter(*filter_list)
+    return render(request, 'orders.html', {'data': {'orders': orders, 'query': input_text}})
 
 def GetOrder(request, id):
-    orders_arr = Orders.objects.all()
-    print(orders_arr    )
-    order = next((find for find in orders_arr if find.id== id), None)
-    if order:
-        print(order.title)
-    else:
-        print("Not found!")
-    
-    return render(request, 'order.html', {'data' : {
-        'orders': order,
-    }})
+    order = Orders.objects.filter(id=id).first()
+    return render(request, 'order.html', {'data': {'orders': order}})
+
